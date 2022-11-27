5e4ad58a43cc62d440b3e938d6e71b75d63fd887
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-11-27 12:18:43 -0500

    Add More Animes

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ import os
 
 from colorama import Fore, Style
 
+animes = (
+    "naruto-shippuuden-dub",
+    "shingeki-no-kyojin-season-2",
+    "shingeki-no-kyojin-season-3",
+    "shingeki-no-kyojin-season-3-part-2",
+)
 
-async def open_episode(episode: int) -> None:
+
+async def open_episode(anime: str, episode: int) -> None:
     """
     Open an episode in the browser.
     """
-    os.system(
-        f"chromium-browser https://animixplay.to/v1/naruto-shippuuden-dub/ep{episode}"
-    )
+    print(f"{Fore.BLUE}Opening {anime} episode {episode}...{Style.RESET_ALL}")
+    os.system(f"chromium-browser https://animixplay.to/v1/{anime}/ep{episode}")
 
 
 async def main() -> None:
@@ -24,23 +30,30 @@ async def main() -> None:
     os.system("git pull")
     print(f"{Fore.GREEN}Pulled latest from GitHub!{Style.RESET_ALL}")
 
-    print(f"Watching {Fore.YELLOW}Naruto Shippuden [Dubbed]{Style.RESET_ALL}")
+    print(f"{Fore.GREEN}Please Enter The Anime you want to Watch{Style.RESET_ALL}")
+    for num, anime in enumerate(animes):
+        print(f"{num} {anime}")
+    anime = animes[int(input("Anime Number: "))]
+
+    print(f"Watching {Fore.YELLOW}{anime}{Style.RESET_ALL}")
 
     if os.path.exists("current.json"):
         with open("current.json", "r", encoding="utf-8") as file:
-            episode = json.loads(file.read())["episode"]
+            full = dict(json.loads(file.read()))
+            episode = full[anime]
+            full.update({anime: episode + 1})
             print(f"{Fore.GREEN}Current episode: {episode}{Style.RESET_ALL}")
         with open("current.json", "w+", encoding="utf-8") as file:
-            json.dump({"episode": episode + 1}, file)
+            json.dump(full, file)
     else:
         with open("current.json", "w+", encoding="utf-8") as file:
             rinput = input(
                 f"{Fore.RED}No current episode found. Please enter a number to start from.{Style.RESET_ALL}: "
             )
             episode = int(rinput)
-            json.dump({"episode": episode + 1}, file)
+            json.dump(full, file)
 
-    asyncio.create_task(open_episode(episode))
+    asyncio.create_task(open_episode(anime, episode))
 
 
 if __name__ == "__main__":
